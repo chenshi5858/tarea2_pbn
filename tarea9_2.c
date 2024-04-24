d86c7146ecbfb1aecd412885ee8f772a00542ecf
chenshi <cshi@miuandes.cl>
2024-04-24 11:49:59 -0400

    tamano variable

--- tarea9_2.c
+++ tarea9_2.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LINE_LENGTH 1000
+#define LARGO_MAX 1000
 
-// Definición del struct para almacenar los datos
 typedef struct {
     int ID;
     int Geoname_ID;
@@ -14,106 +13,224 @@ typedef struct {
     int Elevation;
     char Timezone[50];
     char Coordinates[30];
-} Place;
+} Lugar;
 
-char* strtoke(char *str, const char *delim)
+char* Separador_de_str(char *str, const char *delim)
 {
-    static char *start = NULL; /* stores string str for consecutive calls */
-    char *token = NULL; /* found token */
-    /* assign new start in case */
+    static char *start = NULL;
+    char *token = NULL;
     if (str) start = str;
-    /* check whether text to parse left */
     if (!start) return NULL;
-    /* remember current start as found token */
     token = start;
-    /* find next occurrence of delim */
     start = strpbrk(start, delim);
-    /* replace delim with terminator and move start to follower */
     if (start) *start++ = '\0';
-    /* done */
     return token;
 }
 
-int compareByName(const void* a, const void* b) {
-    const Place* placeA = (const Place*)a;
-    const Place* placeB = (const Place*)b;
-    return strcasecmp(placeA->Name, placeB->Name);
+int Comparar_Nombre(const void* a, const void* b) {
+    const Lugar* lugarA = (const Lugar*)a;
+    const Lugar* lugarB = (const Lugar*)b;
+    return strcasecmp(lugarA->Name, lugarB->Name);
 }
 
-int compareByPopulation(const void* a, const void* b) {
-    const Place* placeA = (const Place*)a;
-    const Place* placeB = (const Place*)b;
+int Comparar_Poblacion(const void* a, const void* b) {
+    const Lugar* lugarA = (const Lugar*)a;
+    const Lugar* lugarB = (const Lugar*)b;
 
-    if (placeA->Population < placeB->Population) {
+    if (lugarA->Population < lugarB->Population) {
         return -1;
-    } else if (placeA->Population > placeB->Population) {
+    } else if (lugarA->Population > lugarB->Population) {
         return 1;
     } else {
         return 0;
     }
 }
 
-// Función para guardar los datos en un struct Place
-void save_data(Place *place, char *fields[]) {
-    place->ID = atoi(fields[0]);
-    place->Geoname_ID = atoi(fields[1]);
-    strcpy(place->Name, strlen(fields[2]) ? fields[2] : "N/A");
-    strcpy(place->Country_Code, strlen(fields[3]) ? fields[3] : "N/A");
-    strcpy(place->Country_name, strlen(fields[4]) ? fields[4] : "N/A");
-    place->Population = strlen(fields[5]) ? atoi(fields[5]) : -1;
-    place->Elevation = strlen(fields[6]) ? atoi(fields[6]) : -1;
-    strcpy(place->Timezone, strlen(fields[7]) ? fields[7] : "N/A");
-    strcpy(place->Coordinates, strlen(fields[8]) ? fields[8] : "N/A");
+int Comparar_Elevacion(const void* a, const void* b) {
+    const Lugar* lugarA = (const Lugar*)a;
+    const Lugar* lugarB = (const Lugar*)b;
+
+    if (lugarA->Elevation < lugarB->Elevation) {
+        return -1;
+    } else if (lugarA->Elevation > lugarB->Elevation) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
-int main() {
-    FILE *file = fopen("bd_final.csv", "r");
-    if (file == NULL) {
-        perror("Error al abrir el archivo");
+int Comparar_Latitud(const void* a, const void* b) {
+    const Lugar* lugarA = (const Lugar*)a;
+    const Lugar* lugarB = (const Lugar*)b;
+
+    char coordenadasA[30], coordenadasB[30];
+    strcpy(coordenadasA, lugarA->Coordinates);
+    strcpy(coordenadasB, lugarB->Coordinates);
+
+    char *tokenA = strtok(coordenadasA, ",");
+    char *tokenB = strtok(coordenadasB, ",");
+
+    double latitudA = atof(tokenA);
+    double latitudB = atof(tokenB);
+
+    if (latitudA < latitudB) {
+        return -1;
+    } else if (latitudA > latitudB) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void Guardar_Datos(Lugar *lugar, char *datos[]) {
+    lugar->ID = atoi(datos[0]);
+    lugar->Geoname_ID = atoi(datos[1]);
+    strcpy(lugar->Name, strlen(datos[2]) ? datos[2] : "N/A");
+    strcpy(lugar->Country_Code, strlen(datos[3]) ? datos[3] : "N/A");
+    strcpy(lugar->Country_name, strlen(datos[4]) ? datos[4] : "N/A");
+    lugar->Population = strlen(datos[5]) ? atoi(datos[5]) : -1;
+    lugar->Elevation = strlen(datos[6]) ? atoi(datos[6]) : -1;
+    strcpy(lugar->Timezone, strlen(datos[7]) ? datos[7] : "N/A");
+    strcpy(lugar->Coordinates, strlen(datos[8]) ? datos[8] : "N/A");
+}
+
+int main(int argc, char **argv) {
+    FILE *archivo = fopen(argv[1], "r");
+    if (argc != 2){
+        printf("Cantidad de argumentos invalida");
+        return 1;
+    }
+    if (archivo == NULL) {
+        printf("Error al abrir el archivo");
         return 1;
     }
 
-    char line[MAX_LINE_LENGTH];
-    int i = 0; // Cambiar a 0 ya que los índices de arreglo comienzan en 0
-    Place *place = malloc(500000 * sizeof(Place)); // Esto debería ser suficiente para almacenar los datos
+    char linea[LARGO_MAX];
+    fgets(linea, LARGO_MAX-1, archivo);
+    int num_lugares = 0;
+    while (fgets(linea, LARGO_MAX-1, archivo)!=NULL) num_lugares++;
+    rewind(archivo);
+    int i = 0;
+    Lugar *lugar = malloc(num_lugares*sizeof(Lugar));
 
-    // Leer la primera línea para ignorar los encabezados
-    fgets(line, MAX_LINE_LENGTH, file);
+    fgets(linea, LARGO_MAX, archivo);
 
-    // Leer y guardar los datos de cada línea del archivo
-    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
-        char *fields[9];
-        char *token = strtoke(line, ";");
+    while (fgets(linea, LARGO_MAX, archivo) != NULL) {
+        char *datos[9];
+        char *token = Separador_de_str(linea, ";");
         int count = 0;
         while (token != NULL && count < 9) {
-            fields[count++] = token;
-            token = strtoke(NULL, ";");
+            datos[count++] = token;
+            token = Separador_de_str(NULL, ";");
         }
         if (count == 9) {
-            save_data(&place[i], fields);
+            Guardar_Datos(&lugar[i], datos);
             i++;
+
+            // printf("ID: %d\n", lugar[i-1].ID);
+            // printf("Geoname ID: %d\n", lugar[i-1].Geoname_ID);
+            // printf("Name: %s\n", lugar[i-1].Name);
+            // printf("Country Code: %s\n", lugar[i-1].Country_Code);
+            // printf("Country Name: %s\n", lugar[i-1].Country_name);
+            // printf("Population: %d\n", lugar[i-1].Population);
+            // printf("Elevation: %d\n", lugar[i-1].Elevation);
+            // printf("Timezone: %s\n", lugar[i-1].Timezone);
+            // printf("Coordinates: %s\n", lugar[i-1].Coordinates);
+            // printf("\n");
         }
     }
-    rewind(file);
-    fgets(line, MAX_LINE_LENGTH - 1, file);
-    int num_lugares = 0;
-    while (fgets(line, MAX_LINE_LENGTH - 1, file) != NULL) num_lugares++;
-    qsort(place, i, sizeof(Place), compareByName);
+
+    //qsort(lugar, i, sizeof(lugar), Comparar_Nombre);
 
     // printf("numero de lugares: %d\n", num_lugares);
-    // for (int j = 0; j < num_lugares; j++) {
-    //     printf("Country Name: %s\n", place[j].Name);
-    //     printf("%d\n", j);
+    // for (int i = 0; i<num_lugares; i++){
+    //     printf("Country Name: %s\n", lugar[i].Name);
+    //     printf("%d\n", i);
+    // }
+    // qsort(lugar, i, sizeof(lugar), Comparar_Poblacion);
+    // for (int i = 0; i<num_lugares; i++){
+    //     printf("Country Name: %d\n", lugar[i].Population);
+    //     printf("%d\n", i);
     // }
 
-    qsort(place, i, sizeof(Place), compareByPopulation);
-    for (int j = 0; j < num_lugares; j++) {
-        printf("Country Name: %s\n", place[j].Name);
-        printf("Population: %d\n", place[j].Population);
-        printf("%d\n", j);
-    }
+    printf("Numero de lugares: %d\n", num_lugares);
+    char input_usuario[50]="vacio";
+    char nombre_ciudad[50];
+    while (strcmp(input_usuario, "SALIR") != 0){
+        printf("Ingrese orden junto al indice (POBL, ELEV, LAT, nombre de la ciudad y SALIR si desea terminar): ");
+        fgets(input_usuario, 49, stdin);
+        input_usuario[strcspn(input_usuario, "\n")] = '\0';
+        strcpy(nombre_ciudad, input_usuario);
+        printf("%s\n", input_usuario);
+        char* token_input = strtok(input_usuario, " ");
+        int respuesta_valida = 0;
+        if(strcmp(token_input, "POBL") == 0){
+            qsort(lugar, i, sizeof(Lugar), Comparar_Poblacion);
+            // for (int i = 0; i<num_lugares; i++){
+            //     printf("Country Name: %s\n", lugar[i].Name);
+            //     printf("Country Population: %d\n", lugar[i].Population);
+            //     printf("%d\n", i);
+            // }
+            token_input=strtok(NULL, " ");
+            if(token_input[0]=='-'){
+                token_input = strtok(token_input, "-");
+                printf("Poblacion de %s, %s: %d habitantes\n", lugar[num_lugares-atoi(token_input)].Name, lugar[num_lugares-atoi(token_input)].Country_name, lugar[num_lugares-atoi(token_input)].Population);
+            }
+            else printf("Poblacion de %s, %s: %d habitantes\n", lugar[atoi(token_input)].Name, lugar[atoi(token_input)].Country_name, lugar[atoi(token_input)].Population);
+            respuesta_valida = 1;
 
-    free(place);
-    fclose(file);
+        }
+        else if(strcmp(token_input, "ELEV") == 0){
+            qsort(lugar, i, sizeof(Lugar), Comparar_Elevacion);
+            // for (int i = 0; i<num_lugares; i++){
+            //     printf("Country Name: %s\n", lugar[i].Name);
+            //     printf("Country Elevation: %d\n", lugar[i].Elevation);
+            //     printf("%d\n", i);
+            // }
+            token_input=strtok(NULL, " ");
+            if(token_input[0]=='-'){
+                token_input = strtok(token_input, "-");
+                printf("Elevacion de %s, %s: %d m.s.n.m\n", lugar[num_lugares-atoi(token_input)].Name, lugar[num_lugares-atoi(token_input)].Country_name, lugar[num_lugares-atoi(token_input)].Elevation);
+            }
+            else printf("Elevacion de %s, %s: %d m.s.n.m\n", lugar[atoi(token_input)].Name, lugar[atoi(token_input)].Country_name, lugar[atoi(token_input)].Elevation);
+            respuesta_valida = 1;
+
+        }
+        else if(strcmp(token_input, "LAT") == 0){
+            token_input=strtok(NULL, " ");
+            qsort(lugar, i, sizeof(Lugar), Comparar_Latitud);
+            if(token_input[0]=='-'){
+                token_input = strtok(token_input, "-");
+                printf("Coordenadas de %s, %s: %s\n", lugar[num_lugares-atoi(token_input)].Name, lugar[num_lugares-atoi(token_input)].Country_name, lugar[num_lugares-atoi(token_input)].Coordinates);
+            }
+            else printf("Coordenadas de %s, %s: %s\n", lugar[atoi(token_input)].Name, lugar[atoi(token_input)].Country_name, lugar[atoi(token_input)].Coordinates);
+            respuesta_valida = 1;
+
+        }
+        else{
+            for (int z = 0; z<num_lugares; z++){
+                if (strcmp(nombre_ciudad, lugar[z].Name) == 0){
+                    printf("Datos de la ciudad %s: \n", nombre_ciudad);
+                    printf("    ID: %d\n", lugar[z].ID);
+                    printf("    Geoname ID: %d\n", lugar[z].Geoname_ID);
+                    printf("    Name: %s\n", lugar[z].Name);
+                    printf("    Country code: %s\n", lugar[z].Country_Code);
+                    printf("    Country name: %s\n", lugar[z].Country_name);
+                    printf("    Population: %d\n", lugar[z].Population);
+                    printf("    Elevation: %d\n", lugar[z].Elevation);
+                    printf("    Timezone: %s\n", lugar[z].Timezone);
+                    printf("    Coordinates: %s\n", lugar[z].Coordinates);
+                    respuesta_valida = 1;
+                    break;
+                }
+            }
+            
+        }
+         
+    if (respuesta_valida == 0 && (strcmp(input_usuario, "SALIR") != 0)) printf("No se encontro el nombre de ciudad ingresado\n\n");
+
+    }
+    free(lugar);
+    fclose(archivo);
     return 0;
 }
